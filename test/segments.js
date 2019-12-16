91b15c5417e86fae98f343e68e97fbab698d1a63
UnfinishedBusiness <travis.gillin@gmail.com>
2019-12-16 10:35:23 -0500

    Acceleration planner is working with test entities here, need to introduce some gcode paths to it

--- test/segments.js
+++ test/segments.js
@@ -33,6 +33,27 @@ function feed_from_distance(accel_rate, distance_into_move)
 function map(x, in_min, in_max, out_min, out_max) {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+function InTolerance(a, b, t)
+{
+  var diff;
+  if (a > b)
+  {
+    diff = a - b;
+  }
+  else
+  {
+    diff = b - a;
+  }
+  //printf("(geoInTolerance) Difference: %.6f, Plus: %.6f, Minus: %.6f\n", diff, fabs(t), -fabs(t));
+  if (diff <= Math.abs(t) && diff >= -Math.abs(t))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 function algorythm()
 {
 	//Calculate point chunks and fill marker_points with chunk points
@@ -64,13 +85,13 @@ function algorythm()
 					//add_point_marker(point);
 				}
 				marker_points.push({ x: entity.end.x, y: entity.end.y });
-				marker_points[marker_points.length-1].marker = {x: entity.end.x, y: entity.end.y};
+				marker_points[marker_points.length-1].marker = {x: marker_points[marker_points.length-1].x, y: marker_points[marker_points.length-1].y};
 			}
 		}
 	}
 	//Itterate through chunk points and calculate the relative angle between last point and next point to determine a directiional change.
 	//Calculate the feedrate that we need to be at once we arive at this point based on the angle change
-	for(var x = 1; x < marker_points.length - 1; x++)
+	for(var x = 1; x < marker_points.length-1; x++)
 	{
 		//console.log("Adding text: " + JSON.stringify(marker_points[x]) + "\n");
 		var P1 = { x: marker_points[x].x, y: marker_points[x].y };
@@ -84,10 +105,11 @@ function algorythm()
 		if (result > 180) result -= 90;
 		if (result < 90) result = 90; //A 90 degree corner will warrent a slowdown to min_feed
 		//console.log("Angle: " + result + "\n");
-		marker_points.angle_change = result;
-		marker_points.max_corner_feed =  map(marker_points.angle_change, 180, 90, target_feed, min_feed);
-		render.add_entity({ type: "text", position: { x: marker_points[x].x, y: marker_points[x].y}, text: marker_points.angle_change.toFixed(2) + ">" + marker_points.max_corner_feed.toFixed(2), height: 0.001 });
+		marker_points[x].angle_change = result;
+		marker_points[x].max_corner_feed =  map(marker_points[x].angle_change, 180, 90, target_feed, min_feed);
+		//render.add_entity({ type: "text", position: { x: marker_points[x].x, y: marker_points[x].y}, text: marker_points[x].angle_change.toFixed(2) + ">" + marker_points[x].max_corner_feed.toFixed(2), height: 0.001 });
 	}
+	marker_points[marker_points.length-1].max_corner_feed = 5; //Make sure we decelerate on last point
 	//Calculate the feedrate based on acceleration value for each chunk and hit each chunks specified feedrate from the last itteration
 	var current_feed = min_feed;
 	var accel_per_chunk = feed_from_distance(accel_rate, chunk_length);
@@ -100,13 +122,50 @@ function algorythm()
 		{
 			current_feed = target_feed;
 		}
-		render.add_entity({ type: "text", position: {x: marker_points[x].x, y: marker_points[x].y - 0.002}, text: current_feed.toFixed(2), height: 0.001 });
+		marker_points[x].current_feed = current_feed;
+	}
+	//We have to iterate the path forwards until we get to a marker. Then itterate backwards until we get to a point were our feedrate peaks for that segment.
+	//Then iterate forward to find the next segment, then itterate backwards until our feedrate peaks, etc
+	var last_marker = 0;
+	for (var x = 0; x < marker_points.length; x++)
+	{
+		if (marker_points[x].marker != undefined)
+		{
+			//Found End Marker
+			console.log("Found Marker - " + marker_points[x].max_corner_feed + "\n");
+			marker_points[x].current_feed = marker_points[x].max_corner_feed;
+			for (var i = x-1; i > last_marker; i--)
+			{
+				marker_points[i].current_feed = marker_points[i+1].current_feed;
+				marker_points[i].current_feed += accel_per_chunk;
+				console.log("\tIterating backwards - this_current_feed: " + marker_points[i].current_feed + ", behind_this_current_feed: " + marker_points[i-1].current_feed  + "\n");
+				if (InTolerance(marker_points[i-1].current_feed, marker_points[i].current_feed, 1))
+				{
+					console.log("\t\tFound Peak!\n");
+					break;
+				}
+			}
+			last_marker = x;
+			//Increment positive from last marker to next marker
+			for (var i = last_marker +1; i < marker_points.length; i++)
+			{
+				marker_points[i].current_feed = marker_points[i-1].current_feed;
+				marker_points[i].current_feed += accel_per_chunk;
+				if (marker_points[i].current_feed > target_feed) marker_points[i].current_feed = target_feed;
+				if (marker_points[i].marker != undefined) break;
+			}
+		}
+	}
+
+	//Only for visualization
+	for (var x = 0; x < marker_points.length; x++)
+	{
+		if (marker_points[x].current_feed != undefined) render.add_entity({ type: "text", position: {x: marker_points[x].x, y: marker_points[x].y - 0.002}, text: "F: " + marker_points[x].current_feed.toFixed(2), height: 0.001 });
 		if (marker_points[x].marker != undefined)
 		{
 			render.add_entity({ type: "text", position: {x: marker_points[x].marker.x, y: marker_points[x].marker.y - 0.004}, text: "marker", height: 0.001 });
 		}
 	}
-
 }
 function setup()
 {

dbcfc384e19e3e2230ea0f5f73263218f310b6bc
UnfinishedBusiness <travis.gillin@gmail.com>
2019-12-30 22:03:48 -0500

    Compiled and is running on windows here

---
+++ src/windows_specific.h
@@ -0,0 +1,18 @@
+#ifndef WINDOWS_SPECIFIC_H
+#define WINDOWS_SPECIFIC_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <cstddef>
+
+size_t getline(char **lineptr, size_t *n, FILE *stream);
+
+#endif

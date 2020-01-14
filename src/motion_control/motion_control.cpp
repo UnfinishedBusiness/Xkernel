349cdea55d455b84ae6c5bc8fe49ae29210dbc08
UnfinishedBusiness <travis.gillin@gmail.com>
2020-01-14 16:43:01 -0500

    Last commit broke soft resets

--- src/motion_control/motion_control.cpp
+++ src/motion_control/motion_control.cpp
@@ -378,7 +378,7 @@ void MotionControl::tick()
     {
         if (this->is_connected_flag == true)
         {
-            this->send_rt("?~"); //Send a status report and cycle start to solve the mysterious and sparatic pause during cuts
+            this->send_rt("?");
             this->dro_interval_timer = this->millis();
         }
     }
